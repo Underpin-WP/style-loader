556d6a7e1e8d520e1cf6f84d5334e3a79b692d73
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:29:19 -0700

    Fixes issues where extended plugins did not get registered items.

--- styles.php
+++ styles.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'Style.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Styles.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Style_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'styles', [
-			'registry' => 'Underpin_Styles\Loaders\Styles',
-		] );
-	}
+add_action( 'underpin/before_setup', function ( $instance ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'Style.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Styles.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Style_Instance.php' );
+	$instance->loaders()->add( 'styles', [
+		'registry' => 'Underpin_Styles\Loaders\Styles',
+	] );
 } );
