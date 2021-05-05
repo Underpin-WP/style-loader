5684b06c4c80a897ef425b525a10528073c8ebb2
Alex Standiford <a@alexstandiford.com>
2021-05-05 15:54:06 -0700

    Corrects underpin autoloader

--- styles.php
+++ styles.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $instance ) {
+add_action( 'underpin/before_setup', function ( $file ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'Style.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Styles.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'Style_Instance.php' );
-	$instance->loaders()->add( 'styles', [
+	Underpin\underpin()->get( $file )->loaders()->add( 'styles', [
 		'registry' => 'Underpin_Styles\Loaders\Styles',
 	] );
 }, 5 );
